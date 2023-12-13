e82af73c257b65bfb742ecb82247be5d55c178f4
VasylTehlivets <vasyltehlivets@gmail.com>
2023-12-13 23:10:15 +0200

    Create Electricity.jsx

---
+++ src/pages/Electricity/Electricity.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Electricity.css";
+
+const Electricity = () => {
+  return (
+    <div>
+      <h2>Electricity we produced for all time</h2>
+          </div>
+  );
+};
+
+export default Electricity;
