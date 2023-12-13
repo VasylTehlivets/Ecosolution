c960f57cf0f377f4937f49745faf5e82e5e1bbb3
VasylTehlivets <vasyltehlivets@gmail.com>
2023-12-13 23:09:11 +0200

    Create Contact.js

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Contact.css";
+
+const Contact = () => {
+  return (
+    <section id="contact">
+      <div className="Phone">38 (098) 12 34 567</div>
+      <div className="Email">office@ecosolution.com</div>
+    </section>
+  );
+};
+
+export default Contact;
