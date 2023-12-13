8cd47c10112cc62c68147ddb28572b35b05ce132
VasylTehlivets <vasyltehlivets@gmail.com>
2023-12-13 23:10:51 +0200

    Create Main.js

---
+++ src/components/Main/Main.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Main.css";
+
+const Main = () => {
+  return (
+    <div>
+      <h1>
+        RENEWABLE ENERGY <br /> For any task
+      </h1>
+      <p>
+        Development and implementation of renewable non-polluting energy
+        sources, generating power generation using energy wind, sun, water,
+        biomass
+      </p>
+      <button>Learn more</button>
+    </div>
+  );
+};
+
+export default Main;
