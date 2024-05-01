860e38f333a5642947f4c905814ab3edae851b9f
VasylTehlivets <vasyltehlivets@gmail.com>
2024-05-01 22:56:22 +0300

    Update Values.js

--- src/pages/Values/Values.js
+++ src/pages/Values/Values.js
@@ -1,29 +1,29 @@
-import React from "react";
-import "./Values.css";
+// import React from "react";
+// import "./Values.css";
 
-const Values = () => {
-  return (
-    <div id="value">
-      <h2>Main values of our company</h2>
-      <p>
-        EcoSolution envisions a world where sustainable energy solutions power a
-        brighter and cleaner future for all. We aspire to be at the forefront of
-        the global shift towards renewable energy, leading the way in innovative
-        technologies that harness the power of nature to meet the world's energy
-        needs.
-      </p>
-      <li>to the world, people, new ideas and projects</li>
-      <li>
-        we are aware that the results of our work have an impact on our lives
-        and the lives of future generations
-      </li>
-      <li>we use the latest technology to implement non-standard solutions</li>
-      <li>
-        we do not strive to be the first among others, but we want to be the
-        best in our business
-      </li>
-    </div>
-  );
-};
+// const Values = () => {
+//   return (
+//     <div id="value">
+//       <h2>Main values of our company</h2>
+//       <p>
+//         EcoSolution envisions a world where sustainable energy solutions power a
+//         brighter and cleaner future for all. We aspire to be at the forefront of
+//         the global shift towards renewable energy, leading the way in innovative
+//         technologies that harness the power of nature to meet the world's energy
+//         needs.
+//       </p>
+//       <li>to the world, people, new ideas and projects</li>
+//       <li>
+//         we are aware that the results of our work have an impact on our lives
+//         and the lives of future generations
+//       </li>
+//       <li>we use the latest technology to implement non-standard solutions</li>
+//       <li>
+//         we do not strive to be the first among others, but we want to be the
+//         best in our business
+//       </li>
+//     </div>
+//   );
+// };
 
-export default Values;
+// export default Values;
